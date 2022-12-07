915fa67bf80ccc026a866e63bfc6de80517f54ab
Vinicius <santoosvinioffice@gmail.com>
2022-12-07 14:26:48 -0300

    HomePage e Navbar
    Primeiras atualizações na HomePage e na Navbar

--- store_management/core/urls.py
+++ store_management/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/<int:id>', views.Index.as_view(), name="index"),
-    path('login', views.login.as_view(), name='login')
+    path('', views.Index.as_view(), name="index"),
+    path('login', views.login.as_view(), name='login'),
 ]
